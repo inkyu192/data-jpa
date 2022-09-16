04b9f8625f23de127b48a0b2e313c3487bf53eac
inkyu192 <inkyu192@gmail.com>
2022-09-16 23:27:12 +0900

    JPA NamedQuery

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -63,14 +63,26 @@ class MemberRepositoryTest {
     @Test
     public void findByUsernameAndAgeGreaterThan() {
         Member m1 = new Member("AAA", 10);
-        Member m2 = new Member("AAA", 20);
+        Member m2 = new Member("BBB", 20);
         memberRepository.save(m1);
         memberRepository.save(m2);
 
-        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("BBB", 15);
 
-        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getUsername()).isEqualTo("BBB");
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testNamedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
     }
 }
